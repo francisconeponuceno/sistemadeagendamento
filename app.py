0847989b5892018813e4b75eaf7d58155614e38e
FRANCISCO NEPONUCENO <120109535+francisconeponuceno@users.noreply.github.com>
2025-09-19 23:27:22 -0300

    atualizacao

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask
+import os
 from config import Config
+from controllers.user_controllers import UserController
+from models.user import db
 
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder=os.path.join('views', 'templates' ))
 app.config.from_object(Config)
 
+# inicializa o banco de dados
+db.init_app(app)
+
+# Criar as tabelas
+with app.app_context():
+    db.create_all()
 
 
+
+app.add_url_rule('/', 'index', UserController.index)
 
 
 if __name__ == '__main__':
---
+++ models/user.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    senha = db.Column(db.String(100), nullable=False)
+    tipo = db.Column(db.String(20), nullable=False)
