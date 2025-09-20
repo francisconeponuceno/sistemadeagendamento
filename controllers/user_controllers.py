825957fe179fae25938da96653ff89c20ad535da
FRANCISCO NEPONUCENO <120109535+francisconeponuceno@users.noreply.github.com>
2025-09-20 15:36:33 -0300

    atualizacao

--- controllers/user_controllers.py
+++ controllers/user_controllers.py
@@ -1,10 +1,25 @@
-from flask import render_template
+from flask import render_template, redirect, request, url_for
+from models.user import User, db
 
 
 class UserController:
     @staticmethod
     def index():
-        return render_template('index.html')
+        users = User.query.all()
+        return render_template('index.html', users=users)
     
+    @staticmethod
     def contact():
+        if request.method == 'POST':
+            nome = request.form['nome']
+            email = request.form['email']
+            senha = request.form['senha']
+            tipo = request.form['useradmin']
+
+            new_user = User(nome=nome, email=email, senha=senha, tipo=tipo)
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect(url_for('index'))
+
         return render_template('contact.html')
