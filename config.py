7670a0499d2a33227dfe52e6bca78810abd0c87e
FRANCISCO NEPONUCENO <120109535+francisconeponuceno@users.noreply.github.com>
2025-09-23 17:53:37 -0300

    atualizacao

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 import secrets
+from dotenv import load_dotenv
 
 #chave = secrets.token_hex()
 
+load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', secrets.token_hex())
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///database.db')
