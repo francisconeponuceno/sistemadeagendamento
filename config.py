19660fcead241e1a6d22d139449311beb47ff23a
FRANCISCO NEPONUCENO <120109535+francisconeponuceno@users.noreply.github.com>
2025-09-23 21:09:55 -0300

    atualizacao

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
+import secrets
+from dotenv import load_dotenv
 
+#chave = secrets.token_hex()
+
+load_dotenv()
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///sistem.db'
+    SECRET_KEY = os.getenv('SECRET_KEY', secrets.token_hex())
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///database.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)
+    
