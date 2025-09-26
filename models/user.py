fdb86c0fa4c0b64c5d93c54958bffd468cea6828
FRANCISCO NEPONUCENO <120109535+francisconeponuceno@users.noreply.github.com>
2025-09-26 15:55:31 -0300

    ATUALIZACAO

--- models/user.py
+++ models/user.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from datetime import datetime, date
+from datetime import date, datetime
 
+hora = datetime.now()
+hora = hora.strftime('%I:%M:%S')
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -12,6 +14,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
+    telefone = db.Column(db.String(20), nullable=False, key='sem telefone')
     senha = db.Column(db.String(100), nullable=False, unique=True)
     tipo = db.Column(db.String(20), nullable=False)
     data_criacao = db.Column(db.Date, default=date.today)
