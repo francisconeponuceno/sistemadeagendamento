c626e458e566518c7b48a1929861f074cc9b46f4
FRANCISCO NEPONUCENO <120109535+francisconeponuceno@users.noreply.github.com>
2025-10-02 12:13:48 -0300

    atualizacao

--- static/js/script.js
+++ static/js/script.js
@@ -2,12 +2,12 @@ function menuShow() {
     let icone = document.querySelector('.menuBuger');
     let menuMobile = document.querySelector('.menu-mobily');
 
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
+    if (menuMobile.classList.contains('menu-active')) {
+        menuMobile.classList.remove('menu-active');
         icone.classList.remove('fa-xmark');
         icone.classList.add('fa-bars');
     } else {
-        menuMobile.classList.add('open');
+        menuMobile.classList.add('menu-active');
         icone.classList.remove('fa-bars');
         icone.classList.add('fa-xmark');
     }
