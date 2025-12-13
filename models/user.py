23abf8e8258a6dce39fededc352ae21ffa81c842
FRANCISCO NEPONUCENO <120109535+francisconeponuceno@users.noreply.github.com>
2025-12-13 13:05:22 -0300

    atualizacao

--- models/user.py
+++ models/user.py
@@ -11,15 +11,22 @@ migrate = Migrate()
 class User(db.Model):
     __tablename__ = "administrador"
 
-    Id_admin = db.Column(db.Integer, primary_key=True)
-    No_admin = db.Column(db.String(100), nullable=False)
-    No_email_admin = db.Column(db.String(100), nullable=False, unique=True)
-    Nu_tel_admin = db.Column(db.String(20), nullable=False)
-    No_senha_admin = db.Column(db.String(100), unique=True)
-    Tp_admin = db.Column(db.String(1), nullable=False, default='A')
-    At_admin = db.Column(db.Integer, nullable=False, default=1)
-    Dt_criacao = db.Column(db.Date, default=date.today)
-    Hr_cadastro = db.Column(db.DateTime, default=datetime.now)
+    Id_Admin = db.Column(db.Integer, primary_key=True)
+    No_Admin = db.Column(db.String(100), nullable=False)
+    Nr_Telefone = db.Column(db.String(20), nullable=False)
+    No_Email = db.Column(db.String(100), nullable=False, unique=True)
+    No_Senha = db.Column(db.String(100), unique=True)
+    Tp_Entrada = db.Column(db.String(1), nullable=False, default='A')
+    Fl_Situacao = db.Column(db.String(1), nullable=False, default='S')
+    Dt_Cadastro = db.Column(db.Date, default=date.today)
+    Hr_Cadastro = db.Column(db.DateTime, default=datetime.now)
+    No_Usualt = db.Column(db.String(50))
+    Dt_Altera = db.Column(db.Date, default=date.today)
+    Hr_Altera = db.Column(db.DateTime, default=datetime.now)
+    No_Usudel = db.Column(db.String(50))
+    Dt_Delete = db.Column(db.Date, default=date.today)
+    Hr_Delete = db.Column(db.DateTime, default=datetime.now)
+    Fl_Delete = db.Column(db.String(1), default="N")
 
 
 # ------------------
@@ -28,18 +35,22 @@ class User(db.Model):
 class Cliente(db.Model):
     __tablename__ = "clientes"
 
-    Id_cliente = db.Column(db.Integer, primary_key=True)
-    No_cliente = db.Column(db.String(100), nullable=False)
-    No_email_cliente = db.Column(db.String(100), nullable=False, unique=True)
-    Nu_tel_cliente = db.Column(db.String(20), nullable=False)
-    No_senha_cliente = db.Column(db.String(100), unique=True)
-    Tp_cliente = db.Column(db.String(20), nullable=False, default='C')
-    At_cliente = db.Column(db.Integer, nullable=False, default=1)
-    Dt_criacao = db.Column(db.Date, default=date.today)
-    Hr_cadastro = db.Column(db.DateTime, default=datetime.now)
-
-    # relacionamento: um cliente pode ter vários agendamentos
-    agendamentos = db.relationship("Agendamento", back_populates="cliente")
+    Id_Cliente = db.Column(db.Integer, primary_key=True)
+    No_Cliente = db.Column(db.String(50), nullable=False)
+    Nr_Telefone = db.Column(db.String(20), nullable=False)
+    No_Email = db.Column(db.String(50), nullable=False)
+    No_Senha = db.Column(db.String(30), unique=True)
+    Tp_Entrada = db.Column(db.String(1), nullable=False, default='C')
+    Fl_Situacao = db.Column(db.String(1), nullable=False, default='S')
+    Dt_Cadastro = db.Column(db.Date, default=date.today)
+    Hr_Cadastro = db.Column(db.DateTime, default=datetime.now)
+    No_Usualt = db.Column(db.String(50))
+    Dt_Altera = db.Column(db.Date, default=date.today)
+    Hr_Altera = db.Column(db.DateTime, default=datetime.now)
+    No_Usudel = db.Column(db.String(50))
+    Dt_Delete = db.Column(db.Date, default=date.today)
+    Hr_Delete = db.Column(db.DateTime, default=datetime.now)
+    Fl_Delete = db.Column(db.String(1), default='N')
 
 
 # --------------------
@@ -48,19 +59,23 @@ class Cliente(db.Model):
 class Profissional(db.Model):
     __tablename__ = 'profissionais'
 
-    Id_profissional = db.Column(db.Integer, primary_key=True)
-    No_profissional = db.Column(db.String(100), nullable=False)
-    Es_profissional = db.Column(db.String(100))
-    No_email_profissional = db.Column(db.String(100), nullable=False, unique=True)
-    Nu_tel_profissional = db.Column(db.String(20), nullable=False)
-    No_senha_profissional = db.Column(db.String(100), unique=True)
-    Tp_profissional = db.Column(db.String(20), nullable=False, default='P')
-    At_profissional = db.Column(db.Integer, nullable=False, default=1)
-    Dt_criacao = db.Column(db.Date, default=date.today)
-    Hr_cadastro = db.Column(db.DateTime, default=datetime.now)
-
-    # relacionamento: um profissional pode ter vários agendamentos
-    agendamentos = db.relationship('Agendamento', back_populates='profissional')
+    Id_Profissional = db.Column(db.Integer, primary_key=True)
+    No_Profissional = db.Column(db.String(50), nullable=False)
+    Nr_Telefone = db.Column(db.String(20), nullable=False)
+    No_Email = db.Column(db.String(100), nullable=False, unique=True)
+    No_Especialidade = db.Column(db.String(50))
+    No_Senha = db.Column(db.String(50), unique=True)
+    Tp_Entrada = db.Column(db.String(1), nullable=False, default='P')
+    Fl_Situacao = db.Column(db.String(1), nullable=False, default='S')
+    Dt_Cadastro = db.Column(db.Date, default=date.today)
+    Hr_Cadastro = db.Column(db.DateTime, default=datetime.now)
+    No_Usualt = db.Column(db.String(50))
+    Dt_Altera = db.Column(db.Date, default=date.today)
+    Hr_Altera = db.Column(db.DateTime, default=datetime.now)
+    No_Usudel = db.Column(db.String(50))
+    Dt_Delete = db.Column(db.Date, default=date.today)
+    Hr_Delete = db.Column(db.DateTime, default=datetime.now)
+    Fl_Delete = db.Column(db.String(1), default="N")
 
 
 # --------------------
@@ -70,15 +85,20 @@ class Profissional(db.Model):
 class Servico(db.Model):
     __tablename__ = 'servicos'
 
-    Id_servico = db.Column(db.Integer, primary_key=True)
-    No_servico = db.Column(db.String(100), nullable=False)
-    Pr_servico = db.Column(db.Float, nullable=False)
-    At_servico = db.Column(db.Integer, nullable=False, default=1)
-    Dt_criacao = db.Column(db.Date, default=date.today)
+    Id_Servico = db.Column(db.Integer, primary_key=True)
+    No_Servico = db.Column(db.String(100), nullable=False)
+    Pr_Servico = db.Column(db.Float, nullable=False)
+    Tp_Estimado = db.Column(db.String(15))
+    Fl_Situacao = db.Column(db.String(1), nullable=False, default='S')
+    Dt_Cadastro = db.Column(db.Date, default=date.today)
     Hr_cadastro = db.Column(db.DateTime, default=datetime.now)
-
-    # relacionamento: um serviço pode estar em vários agendamentos
-    agendamentos = db.relationship('Agendamento', back_populates='servico')
+    No_Usualt = db.Column(db.String(50))
+    Dt_Altera = db.Column(db.Date, default=date.today)
+    Hr_Altera = db.Column(db.DateTime, default=datetime.now)
+    No_Usudel = db.Column(db.String(50))
+    Dt_Delete = db.Column(db.Date, default=date.today)
+    Hr_Delete = db.Column(db.DateTime, default=datetime.now)
+    Fl_Delete = db.Column(db.String(1), default="N")
 
 
 # --------------------
@@ -88,16 +108,20 @@ class Servico(db.Model):
 class Agendamento(db.Model):
     __tablename__ = 'agendamentos'
 
-    Id_agendamento = db.Column(db.Integer, primary_key=True)
-    cliente_id = db.Column(db.Integer, db.ForeignKey('clientes.Id_cliente'),nullable=False)
-    profissional_id = db.Column(db.Integer, db.ForeignKey('profissionais.Id_profissional'),nullable=False)
-    servico_id = db.Column(db.Integer, db.ForeignKey('servicos.Id_servico'), nullable=False)
-
-    Dt_agendamento = db.Column(db.Date, nullable=False)
-    Hr_agendamento = db.Column(db.Time, nullable=False)
-    status_agendamento = db.Column(db.String(20), default='pendente')
-
-    # RELACIONAMENTOS REVERSOS
-    cliente = db.relationship('Cliente', back_populates='agendamentos')
-    profissional = db.relationship('Profissional', back_populates='agendamentos')
-    servico = db.relationship('Servico', back_populates='agendamentos')
+    Id_Agendamento = db.Column(db.Integer, primary_key=True)
+    No_Cliente = db.Column(db.String(50),nullable=False)
+    No_Profissional = db.Column(db.String(50),nullable=False)
+    No_Servico = db.Column(db.String(50), nullable=False)
+    Dt_Agendamento = db.Column(db.Date, nullable=False)
+    Hr_Agendamento = db.Column(db.Time, nullable=False)
+    Status_agendamento = db.Column(db.String(20), default='pendente')
+    Fl_Situacao = db.Column(db.String(1), nullable=False, default="S")
+    Dt_Cadastro = db.Column(db.Date, default=date.today)
+    Hr_cadastro = db.Column(db.DateTime, default=datetime.now)
+    No_Usualt = db.Column(db.String(50))
+    Dt_Altera = db.Column(db.Date, default=date.today)
+    Hr_Altera = db.Column(db.DateTime, default=datetime.now)
+    No_Usudel = db.Column(db.String(50))
+    Dt_Delete = db.Column(db.Date, default=date.today)
+    Hr_Delete = db.Column(db.DateTime, default=datetime.now)
+    Fl_Delete = db.Column(db.String(1), default="N")
