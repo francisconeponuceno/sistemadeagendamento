25b260d6f55bc3cf21d80fe4e29e0f6c970083e2
FRANCISCO NEPONUCENO <120109535+francisconeponuceno@users.noreply.github.com>
2025-12-16 22:24:42 -0300

    atualizacao

--- controllers/user_controllers.py
+++ controllers/user_controllers.py
@@ -7,7 +7,7 @@ class UserController:
     def index():
         users = User.query.all()
         return render_template('index.html', users=users)
-    
+
     @staticmethod
     def login():
         if request.method == 'POST':
@@ -27,15 +27,14 @@ class UserController:
             return redirect(url_for('index'))
 
         return render_template('login.html')
-    
+
     @staticmethod
     def cadastro():
         return render_template('cadastro.html')
-    
+
     @staticmethod
     def cliete():
         return render_template('cliente.html')
-    
 
     # ROTA DO ADMINISTRADOR
     @staticmethod
@@ -46,19 +45,16 @@ class UserController:
     def agendar():
         ContEtapa = 1
         return render_template('agendar.html',ContEtapa=ContEtapa)
-    
-    #ROTA CADASTRO DE PROFISSIONAL
+
+    # ROTA CADASTRO DE PROFISSIONAL
     @staticmethod
     def CadProfissional():
         if request.method == 'POST':
-            nome = request.form['nome']
-            especialidade = request.form['especialidade']
-            horadio_atendimento = request.form['h_atendimento']
-            telefone = request.form['telefone']
-            new_profissional = Profissional(No_Profissional=nome, Nr_Telefone=telefone, Hr_Atendimento=horadio_atendimento, No_Especialidade=especialidade,)
-            db.session.add(new_profissional)
-            db.session.commit()
-            return render_template('admin.html')
-
-
-
+            return render_template("admin.html")
+            #nome = request.form['nome']
+            #especialidade = request.form['especialidade']
+            #horadio_atendimento = request.form['h_atendimento']
+            #telefone = request.form['telefone']
+            #new_profissional = Profissional(No_Profissional=nome, Nr_Telefone=telefone, Hr_Atendimento=horadio_atendimento, No_Especialidade=especialidade,)
+            #db.session.add(new_profissional)
+            #db.session.commit()
