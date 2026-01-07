dd7577724531de5262a48421419129542e6a9b14
FRANCISCO NEPONUCENO <120109535+francisconeponuceno@users.noreply.github.com>
2026-01-07 10:06:58 -0300

    atualiacao

--- config.py
+++ config.py
@@ -8,5 +8,6 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', secrets.token_hex())
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///database.db')
+    DEBUG_TB_ENABLED = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
